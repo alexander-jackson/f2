8231d543d8b00093f55b1a89e803efdcf80de46d
Alexander Jackson <alex.jackson@starlingbank.com>
2022-09-11 15:09:52 +0100

    feat: swap to handwritten Docker API library
    `cargoship` is a handwritten Docker API library custom to this project,
    and currently only exists locally. In the next commit, it will be
    inlined into the project as it is a little rough around the edges and
    does not provide much functionality.
    
    This allows everything to be queried as expected, including pulling the
    images and starting containers with the correct port mappings.

--- src/docker.rs
+++ src/docker.rs
@@ -1,7 +1,6 @@
+use std::collections::HashMap;
+
 use anyhow::Result;
-use docker_api::container::{ContainerCreateOpts, PublishPort};
-use docker_api::network::PortMap;
-use docker_api::Docker;
 
 use crate::common::{Container, Registry};
 
@@ -11,54 +10,63 @@ pub async fn create_and_start_on_random_port(
     registry: &Registry,
     tag: &str,
 ) -> Result<u16> {
-    let client = Docker::unix("/var/run/docker.sock");
+    let client = cargoship::Client::new("/var/run/docker.sock");
+
+    // Ensure the image exists locally
+    pull_image_if_needed(&client, &container, &registry, tag).await?;
 
     // Create the container
-    let name = format!("{}:{}", container.image, tag);
+    let name = format!("{}/{}:{}", registry.repository, container.image, tag);
+    let target_port = container.target_port;
 
-    // Ensure the container exists locally
-    pull_image_if_needed().await?;
+    let mut exposed_ports = HashMap::new();
+    exposed_ports.insert(target_port, 0);
 
-    let container_options = ContainerCreateOpts::builder(&name)
-        .expose(PublishPort::tcp(container.target_port as u32), 0)
-        .build();
+    let id = client.create_container(&name, &exposed_ports).await?;
+    client.start_container(&id).await?;
 
-    let res = client.containers().create(&container_options).await?;
-    let container_id = res.id();
-
-    let docker_container = docker_api::Container::new(client.clone(), container_id);
-    docker_container.start().await?;
-
-    tracing::info!(%container_id, %name, %container.target_port, "Created and started a container");
+    tracing::info!(%id, %name, %target_port, "Created and started a container");
 
     // Get the container itself and the port details
-    let details = client.containers().get(container_id).inspect().await?;
-    let ports = details.network_settings.ports;
-
-    let binding =
-        find_binding(container.target_port, ports).expect("Failed to find a port mapping");
+    let exposed_ports = client.get_exposed_ports(&id).await?;
+    let binding = *exposed_ports
+        .get(&target_port)
+        .expect("Failed to find a port mapping");
 
     tracing::info!(%container.image, %tag, %binding, "Found the binding for a container");
 
     Ok(binding)
 }
 
-async fn pull_image_if_needed() -> Result<()> {
-    Ok(())
-}
+async fn pull_image_if_needed(
+    client: &cargoship::Client,
+    container: &Container,
+    registry: &Registry,
+    tag: &str,
+) -> Result<()> {
+    // Check whether we have the image locally
+    let expected_tag = format!("{}/{}:{}", registry.repository, container.image, tag);
 
-fn find_binding(port: u16, mapping: Option<PortMap>) -> Option<u16> {
-    mapping?
+    let local_images = client.fetch_images().await?;
+
+    // Find all the ones with tags
+    let exists = local_images
         .iter()
-        .filter(|(k, _)| k.split_once('/').unwrap().0.parse() == Ok(port))
-        .map(|(_, v)| {
-            v.as_ref()
-                .unwrap()
-                .first()
-                .unwrap()
-                .host_port
-                .parse()
-                .unwrap()
-        })
-        .next()
+        .any(|image| image.repo_tags.contains(&expected_tag));
+
+    if exists {
+        tracing::info!(?container, ?registry, %tag, "Image already exists locally");
+        return Ok(());
+    }
+
+    tracing::info!(?container, ?registry, %tag, "Image does not exist locally, pulling from repository");
+
+    // Pull the image from the remote
+    client
+        .pull_image(&registry.repository, &container.image, tag)
+        .await?;
+
+    tracing::info!(?container, ?registry, %tag, "Successfully pulled the image from the repository");
+
+    Ok(())
 }
