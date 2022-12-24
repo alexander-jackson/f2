d443a89acd34e8958a9646c5584dd894b6bd9095
Alexander Jackson <alex.jackson@starlingbank.com>
2022-12-24 19:35:55 +0000

    refactor: improve load balancer error handling
    Turns out `anyhow::Error` is suitable for returning in a
    `hyper::Service`, so let's use that so we get some nicer error handling
    in places and can remove some of the `expect` calls with a `?` instead.

--- src/load_balancer.rs
+++ src/load_balancer.rs
@@ -2,11 +2,11 @@ use std::collections::HashMap;
 use std::net::{Ipv4Addr, SocketAddrV4};
 use std::sync::Arc;
 
-use anyhow::Result;
+use anyhow::{Context, Error, Result};
 use hyper::client::HttpConnector;
 use hyper::http::uri::PathAndQuery;
 use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Client, Error, Request, Response, Server};
+use hyper::{Body, Client, Request, Response, Server};
 use rand::prelude::{SeedableRng, SliceRandom, SmallRng};
 use tokio::sync::Mutex;
 
@@ -18,7 +18,7 @@ type ServiceMap = HashMap<Service, Vec<u16>>;
 
 #[derive(Clone, Debug)]
 pub struct LoadBalancer {
-    port: u16,
+    base_port: u16,
     registry: Arc<Registry>,
     service_map: Arc<ServiceMap>,
     client: Client<HttpConnector>,
@@ -26,14 +26,14 @@ pub struct LoadBalancer {
 }
 
 impl LoadBalancer {
-    pub fn new(port: u16, registry: Registry, service_map: ServiceMap) -> Self {
+    pub fn new(base_port: u16, registry: Registry, service_map: ServiceMap) -> Self {
         let registry = Arc::new(registry);
         let service_map = Arc::new(service_map);
         let client = Client::new();
         let rng = Arc::new(Mutex::new(SmallRng::from_entropy()));
 
         Self {
-            port,
+            base_port,
             registry,
             service_map,
             client,
@@ -43,16 +43,16 @@ impl LoadBalancer {
 
     pub async fn start(&mut self) -> Result<()> {
         // Create the server itself on the given port
-        let addr = SocketAddrV4::new(Ipv4Addr::LOCALHOST, self.port).into();
+        let addr = SocketAddrV4::new(Ipv4Addr::LOCALHOST, self.base_port).into();
 
         let service_map = Arc::clone(&self.service_map);
-        let client = self.client.clone();
         let rng = Arc::clone(&self.rng);
+        let client = self.client.clone();
 
         let service = make_service_fn(move |_| {
-            let client = client.clone();
-            let rng = Arc::clone(&rng);
             let service_map = Arc::clone(&service_map);
+            let rng = Arc::clone(&rng);
+            let client = client.clone();
 
             async move {
                 Ok::<_, Error>(service_fn(move |req| {
@@ -105,30 +105,30 @@ async fn proxy_request_downstream(
     let host = req
         .headers()
         .get(hyper::header::HOST)
-        .expect("Failed to get `host` header")
-        .to_str()
-        .expect("Invalid header supplied");
+        .context("Failed to get `host` header")?
+        .to_str()?;
 
     let downstreams = service_map
         .iter()
         .find_map(|(service, downstreams)| (service.host == host).then_some(downstreams))
-        .expect("Failed to find downstream hosts");
+        .context("Failed to find downstream hosts")?;
 
-    let mut rng = rng.lock().await;
+    let downstream = {
+        let mut rng = rng.lock().await;
 
-    let downstream = *downstreams
-        .choose(&mut *rng)
-        .expect("No available downstreams");
+        *downstreams
+            .choose(&mut *rng)
+            .context("Failed to select downstream host")?
+    };
 
     let downstream_addr = SocketAddrV4::new(Ipv4Addr::LOCALHOST, downstream);
-
     let path = req.uri().path_and_query().map_or("/", PathAndQuery::as_str);
 
     tracing::info!(%downstream_addr, %path, "Proxing request to a downstream server");
 
-    *req.uri_mut() = format!("http://{downstream_addr}{path}").parse().unwrap();
+    *req.uri_mut() = format!("http://{downstream_addr}{path}").parse()?;
 
-    client.request(req).await
+    Ok(client.request(req).await?)
 }
 
 #[tracing::instrument]
