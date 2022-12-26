a7d5e40a2dfdb2f7aba349fff35c764639113e17
Alexander Jackson <alex.jackson@starlingbank.com>
2022-12-26 18:19:45 +0000

    refactor: move LB tests to separate file
    The tests for the load balancer are quite long due to the amount of
    setup, so let's move them to a separate file.
    
    This change:
    * Moves the LB tests to their own dedicated file

---
+++ src/load_balancer/tests.rs
@@ -0,0 +1,169 @@
+use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4, TcpListener};
+
+use anyhow::{Error, Result};
+use hyper::header::HOST;
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Client, Request, Response, Server, StatusCode};
+
+use crate::common::Registry;
+use crate::config::Service;
+use crate::load_balancer::LoadBalancer;
+
+fn some_registry() -> Registry {
+    Registry {
+        base: None,
+        repository: String::from("blah"),
+        username: None,
+        password: None,
+    }
+}
+
+fn create_service(host: &'static str) -> Service {
+    Service {
+        app: String::from("application"),
+        tag: String::from("20220813-1803"),
+        port: 6500,
+        replicas: 1,
+        host: String::from(host),
+    }
+}
+
+async fn handler(response: &'static str) -> Result<Response<Body>> {
+    Ok(Response::new(Body::from(response)))
+}
+
+async fn handle_health_checks(req: Request<Body>) -> Result<Response<Body>> {
+    let response = match req.uri().path() {
+        "/health" => Response::new(Body::empty()),
+        _ => Response::builder().status(404).body(Body::empty())?,
+    };
+
+    Ok(response)
+}
+
+async fn spawn_fixed_response_server(response: &'static str) -> Result<SocketAddr> {
+    let addr = SocketAddrV4::new(Ipv4Addr::LOCALHOST, 0);
+    let listener = TcpListener::bind(&addr)?;
+
+    let service =
+        make_service_fn(
+            move |_| async move { Ok::<_, Error>(service_fn(move |_| handler(response))) },
+        );
+
+    let resolved_addr = listener.local_addr()?;
+
+    tokio::spawn(async move {
+        let server = Server::from_tcp(listener)
+            .expect("Failed to create server")
+            .serve(service);
+
+        server.await.expect("Failed to run server");
+    });
+
+    Ok(resolved_addr)
+}
+
+async fn spawn_load_balancer(service_map: super::ServiceMap) -> Result<SocketAddr> {
+    let addr = SocketAddrV4::new(Ipv4Addr::LOCALHOST, 0);
+    let listener = TcpListener::bind(&addr)?;
+
+    let resolved_addr = listener.local_addr()?;
+
+    tokio::spawn(async move {
+        let registry = some_registry();
+        let mut load_balancer = LoadBalancer::new(registry, service_map);
+
+        load_balancer
+            .start(listener)
+            .await
+            .expect("Failed to run load balancer");
+    });
+
+    Ok(resolved_addr)
+}
+
+#[tokio::test]
+async fn can_proxy_requests_based_on_host_header() -> Result<()> {
+    let opentracker_addr = spawn_fixed_response_server("Hello from OpenTracker").await?;
+    let blackboards_addr = spawn_fixed_response_server("Hello from Blackboards").await?;
+
+    let service_map = [
+        (
+            create_service("opentracker.app"),
+            vec![opentracker_addr.port()],
+        ),
+        (
+            create_service("blackboards.pl"),
+            vec![blackboards_addr.port()],
+        ),
+    ]
+    .into_iter()
+    .collect();
+
+    let load_balancer_addr = spawn_load_balancer(service_map).await?;
+
+    let client = Client::new();
+
+    let request = Request::builder()
+        .uri(format!("http://{}", load_balancer_addr))
+        .header(HOST, "blackboards.pl")
+        .body(Body::empty())?;
+
+    let mut response = client.request(request).await?;
+    let bytes = hyper::body::to_bytes(response.body_mut()).await?;
+    let body = std::str::from_utf8(&bytes)?;
+
+    assert_eq!(body, "Hello from Blackboards");
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn request_paths_are_proxied_downstream() -> Result<()> {
+    let host = "opentracker.app";
+
+    let addr = SocketAddrV4::new(Ipv4Addr::LOCALHOST, 0);
+    let listener = TcpListener::bind(&addr)?;
+
+    let service = make_service_fn(move |_| async move {
+        Ok::<_, Error>(service_fn(move |req| handle_health_checks(req)))
+    });
+
+    let resolved_addr = listener.local_addr()?;
+
+    tokio::spawn(async move {
+        let server = Server::from_tcp(listener)
+            .expect("Failed to create server")
+            .serve(service);
+
+        server.await.expect("Failed to run server");
+    });
+
+    let service_map = [(create_service(host), vec![resolved_addr.port()])]
+        .into_iter()
+        .collect();
+
+    let load_balancer_addr = spawn_load_balancer(service_map).await?;
+
+    let client = Client::new();
+
+    let request = Request::builder()
+        .uri(format!("http://{}/health", load_balancer_addr))
+        .header(HOST, host)
+        .body(Body::empty())?;
+
+    let response = client.request(request).await?;
+
+    assert_eq!(response.status(), StatusCode::OK);
+
+    let request = Request::builder()
+        .uri(format!("http://{}/something-else", load_balancer_addr))
+        .header(HOST, host)
+        .body(Body::empty())?;
+
+    let response = client.request(request).await?;
+
+    assert_eq!(response.status(), StatusCode::NOT_FOUND);
+
+    Ok(())
+}
