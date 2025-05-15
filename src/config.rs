31ff262447f176e0adaca90cc28603821bf129b5
Alexander Jackson <alexanderjackson@protonmail.com>
2025-05-15 12:05:03 +0100

    feat: use lowercase for shutdown mode (#73)
    The `ShutdownMode` currently deserialises from `Graceful`, but it should
    probably use `graceful` for nicer formatting.
    
    This change:
    * Adds the required annotation

--- src/config.rs
+++ src/config.rs
@@ -196,6 +196,7 @@ impl Deref for ReplicaCount {
 }
 
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Deserialize)]
+#[serde(rename_all = "lowercase")]
 pub enum ShutdownMode {
     Graceful,
     Forceful,
