f326cfe95a65936ce85f33175722c11d7bbe089d
Alexander Jackson <alexanderjackson@protonmail.com>
2025-06-20 07:34:52 +0100

    feat: remove legacy setup

--- scripts/run-in-docker.sh
+++ scripts/run-in-docker.sh
@@ -13,12 +13,3 @@ docker run -it \
 	--network mesh \
 	--env-file .env \
 	f2:debug -- --config /tmp/config.yaml
-
-docker run -it \
-	-p 3000:3000 \
-	-v ./f2.yaml:/tmp/config.yaml \
-	-v ./forkup.yaml:/app/forkup.yaml \
-	-v /tmp:/tmp \
-	-v /var/run/docker.sock:/var/run/docker.sock \
-	--env-file .env \
-	f2:debug -- --config /tmp/config.yaml
